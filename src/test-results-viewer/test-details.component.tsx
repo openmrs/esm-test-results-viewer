bcc7a31f9dddda17c5b761e9b090c2976c6033c6
ayush.sharma <ayush.sharma@thoughtworks.com>
2020-10-07 19:03:28 +0530

    #0000 Aashu, Ayush: add style to test-details component

--- src/test-results-viewer/test-details.component.tsx
+++ src/test-results-viewer/test-details.component.tsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { TestResult } from "./test-results-viewer.types";
+import * as styles from "./test-details.component.css";
 
 export default function TestDetailsComponent(props: any) {
+  const isResultHigh = props.testDetails.result == TestResult.High;
   return (
-    <div style={{ display: "flex" }}>
+    <div className={styles.testDetail}>
       <div>{props.testDetails.name}</div>
-      <div>{props.testDetails.value}</div>
+      <div
+        className={`${isResultHigh ? `${styles.highRange} omrs-bold` : ""} `}
+      >
+        <span>{props.testDetails.value}</span>
+        {isResultHigh && (
+          <span>
+            <svg
+              className="omrs-icon"
+              fill="var(--omrs-color-danger)"
+              style={{ height: "1rem", width: "1rem" }}
+            >
+              <use xlinkHref="#omrs-icon-arrow-upward"></use>
+            </svg>
+          </span>
+        )}
+      </div>
       <div>{props.testDetails.range}</div>
     </div>
   );
